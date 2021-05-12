f3e67e1f8be9492dce7c40c5176297e82b8fc8b8
HibernationNo1 <winter4958@gmail.com>
2021-05-12 22:51:05 +0900

    add tf.argmax, conmat

--- project directory/utils/train_validation_test.py
+++ project directory/utils/train_validation_test.py
@@ -10,7 +10,7 @@ def go_train(train_ds, model, loss_object, optimizer, metric_objects):
         # backward propagation에서 계산에 필요한 값을 저장하기 위해 GradientTape 사용
             predictions = model(images)
             loss = loss_object(labels, predictions)
-            # loss를 계산
+            # SCCE 로 loss를 계산
            
 
         gradients = tape.gradient(loss, model.trainable_variables)
@@ -21,15 +21,13 @@ def go_train(train_ds, model, loss_object, optimizer, metric_objects):
         metric_objects['train_loss'](loss)
         # train_loss 안에는 Mean() 이라는 함수가 들어있다. 
         # 즉, Mean(loss) 가 동작하는 code임. cost를 계산.
-        # 이를 통해 back propagation이 진행될 때 derivative를 구하기 위한 정보를
-        # Mean()을 호출한 instance인 train_loss가 가질 수 있는 것이다.
         metric_objects['train_acc'](labels, predictions)
         # SparseCategoricalAccuracy(labels, predictions)
         
 
 # ---------- validation data로 validation하는 함수 -----------
 @tf.function
-def go_validation(validation_ds, model, loss_object, metric_objects):
+def go_validation(validation_ds, model, loss_object, metric_objects, con_mat):
     # optimizer 안할거라 Tape 불필요
     for images, labels in validation_ds:
         images = tf.cast(images, dtype = tf.float32)
@@ -39,7 +37,14 @@ def go_validation(validation_ds, model, loss_object, metric_objects):
         metric_objects['validation_loss'](loss)
         metric_objects['validation_acc'](labels, predictions)
 
+        predictions_one_hot = tf.argmax(predictions, axis = 1)
+        con_mat += tf.math.confusion_matrix(labels, predictions_one_hot)
+    
+        
+        
+
 # ----------test data로 test해보고 그 결과를 저장하는 함수 -----------
+import sys
 @tf.function
 def go_test(test_ds, model, loss_object, metric_objects, path_dict):
     for images, labels in test_ds:
@@ -49,8 +54,9 @@ def go_test(test_ds, model, loss_object, metric_objects, path_dict):
 
         metric_objects['test_loss'](loss)
         metric_objects['test_acc'](labels, predictions)
-        
-    loss, acc = metric_objects['test_loss'].result().numpy(), metric_objects['test_acc'].result()
+    
+    loss, acc = metric_objects['test_loss'].result(), metric_objects['test_acc'].result()
+
     with open(path_dict['cp_path'] + '/test_result.txt', 'w') as f:
-        # 경로 위에 있는 file 열어주고
+        # 경로 위에 있는 file 열어주고 (경로 위에 file이 없다면 새로 생성)
         f.write(f"test_loss: {loss} \ntest_acc: {acc*100}")
