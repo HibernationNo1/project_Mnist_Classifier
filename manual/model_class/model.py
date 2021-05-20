2fa4b7dddec5b21dc9d5b7f213faf5fe0b40b8ea
HibernationNo1 <winter4958@gmail.com>
2021-05-21 03:30:06 +0900

    change keras to Imple

--- manual/model_class/model.py
+++ manual/model_class/model.py
@@ -1,37 +1,37 @@
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.models import Model
 
-import dense, flatten
+from Fully_connected import Dense, Flatten
+from Feature_Extractor import Conv2D, MaxPooling2D
 
 class MnistClassifier(Model):
     def __init__(self):
         super(MnistClassifier, self).__init__()
 
         # feature extractor
-
-
-        self.fe.add(Conv2D(filters = 8, kernel_size = 5, padding = 'same', activation = 'relu'))  
+        self.Conv2D1 = Conv2D(filters = 8, kernel_size = 5, padding = 'same')
         # (None, 28, 28, 8)
-        self.fe.add(MaxPooling2D(pool_size = 2, strides = 2))
+        self.MaxPool1 = MaxPooling2D(pool_size = 2, strides = 2)
         # (None, 14, 14, 8)
-        self.fe.add(Conv2D(filters = 32, kernel_size = 5, padding = 'same', activation = 'relu'))  
+        self.Conv2D2 = Conv2D(filters = 32, kernel_size = 5, padding = 'same', activation = 'relu')
         # (None, 14, 14, 32)
-        self.fe.add(MaxPooling2D(pool_size = 2, strides = 2))
+        self.MaxPool2 = MaxPooling2D(pool_size = 2, strides = 2)
         # (None, 7, 7, 32)
 
-        self.Flatten = flatten()
+        self.Flatten = Flatten()
         # (None, 1568)       
        
 
     def call(self, x):
-        x = self.fe(x)
-        
+        x = self.Conv2D1(x)
+        x = self.MaxPool1(x)
+        x = self.Conv2D2(x)
+        x = self.MaxPool2(x)
 
         x = self.Flatten(x)
         
-        self.Dense_relu = dense(x.shape[1], units = 64)
-        x = self.Dense_relu(x)
-        self.softmax = dense(x.shape[1], units = 10)
-        x = self.softmax(x)
+        self.Dense_relu = Dense(x.shape[1], units = 64)
+        x = self.Dense_relu.relu(x)
+        self.Dense_softmax = Dense(x.shape[1], units = 10)
+        x = self.Dense_softmax.softmax(x)
 
         return x
